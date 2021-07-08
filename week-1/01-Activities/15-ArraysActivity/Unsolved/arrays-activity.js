1ca1d1f0a7f1abdd551cffbb0c92338b0e70993f
Frank <dev.frankg@gmail.com>
2021-07-08 14:28:03 -0400

    adding arrays activity

---
+++ week-1/01-Activities/15-ArraysActivity/Unsolved/arrays-activity.js
@@ -0,0 +1,28 @@
+
+      // Fill in the question marks with what the output will be.
+      var animals = ["parrot", "cat", "dog"];
+
+      // ???
+      console.log(animals.length);
+
+      // ???
+      console.log(animals[1]);
+
+      // ???
+      console.log(animals[2]);
+
+      // ???
+      console.log(animals[0]);
+
+      // ???
+      console.log(animals[animals.length]);
+
+      // ???
+      console.log(animals[animals.length - 1]);
+
+      // ???
+      console.log(animals.indexOf("bear"));
+
+      // ???
+      console.log(animals.indexOf("parrot"));
+
