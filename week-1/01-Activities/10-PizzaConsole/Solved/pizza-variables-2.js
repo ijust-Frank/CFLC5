7b3df29e97ee4459bff9fb21788ebf74ba1fb53d
Frank <dev.frankg@gmail.com>
2021-07-08 13:26:44 -0400

    adding pizza console solved and conditional activity2

---
+++ week-1/01-Activities/10-PizzaConsole/Solved/pizza-variables-2.js
@@ -0,0 +1,15 @@
+
+    // Variables
+    var name = "Joe Frasier";
+    var pizzaCount = 15;
+    var isHungry = true;
+    var totalCost = pizzaCount * 5.00;
+
+    // Go to Chrome -> Inspector -> Console to view the outputs
+    console.log("Welcome: " + name);
+
+    console.log("Pizzas cost $5 each");
+
+    console.log("Your total is: $" + totalCost);
+
+    console.log("Still Hungry: " + isHungry);
---
+++ week-1/01-Activities/13-ConditionalActivity2/conditional-demo-2.js
@@ -0,0 +1,55 @@
+      if (true === true) {
+        console.log("hi");
+      }
+      else {
+        console.log("bye");
+      }
+
+      if (false === false) {
+        console.log("dude");
+      }
+      else {
+        console.log("nope");
+      }
+
+      if ("4" === 4) {
+        console.log("puppies");
+      }
+      else {
+        console.log("goats");
+      }
+
+      if ("4" == 4) {
+        console.log("puppies");
+      }
+      else {
+        console.log("goats");
+      }
+
+      if (true) {
+        console.log("goo goo dolls");
+      }
+      else {
+        console.log("vanilla ice");
+      }
+
+      if (false) {
+        console.log("the neighborhood");
+      }
+      else {
+        console.log("one direction");
+      }
+
+      if (("5" + "5") === "55") {
+        console.log("tofu");
+      }
+      else {
+        console.log("soybean");
+      }
+
+      if ((6 + "5") === "65") {
+        console.log("richard");
+      }
+      else {
+        console.log("branson");
+      }
