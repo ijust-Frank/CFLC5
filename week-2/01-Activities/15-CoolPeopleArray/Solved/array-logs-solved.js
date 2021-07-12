318f3e5ac9f04c97de44f8aa7776bcd4d25f1ddf
Frank <dev.frankg@gmail.com>
2021-07-12 09:40:56 -0400

    adding Array setting activity + coolpeople activity solution

---
+++ week-2/01-Activities/15-CoolPeopleArray/Solved/array-logs-solved.js
@@ -0,0 +1,11 @@
+
+    // This array contains our list of cool people.
+    var coolPeople = ["bruce lee", "arnold schwarzenegger", "will smith", "esther", "marie curie", "joan of arc"];
+
+    // We can log an individual item from an array by referencing it's index in the following fashion.
+    console.log(coolPeople[0]);
+    console.log(coolPeople[1]);
+    console.log(coolPeople[2]);
+    console.log(coolPeople[3]);
+    console.log(coolPeople[4]);
+    console.log(coolPeople[5]);
---
+++ week-2/01-Activities/16-ArraySetting/Unsolved/array-setting-unsolved.js
@@ -0,0 +1,22 @@
+
+
+    // Create an Array of Drinks with all capital letters
+    // ...
+    var drinks = ["Coke", "Pepsi", "Water", "Orange Juice"];
+    // ...
+
+    console.log("First statement");
+    console.log(drinks[0]);
+    console.log(drinks[1]);
+    console.log(drinks[2]);
+    console.log(drinks[3]);
+
+    // Then modify each element of the array (individually) to make them lowercase
+    // ...
+
+
+    // CODE GOES HERE
+
+
+    // ...
+
