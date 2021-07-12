db5b32421a4f43dfcd2bcc3514941309f8bcf935
Frank <dev.frankg@gmail.com>
2021-07-11 21:33:58 -0400

    adding dissect activity

---
+++ week-2/01-Activities/14-JSDissect/Unsolved/js-dissect-unsolved.js
@@ -0,0 +1,49 @@
+
+
+      var userName = "player1";
+      var confirmCats = true;
+
+      var petName = "Meeses";
+      var petType = "Tuxedo Cat";
+      var petAge = 5;
+      var petIsCat = true;
+
+      if (confirmCats) {
+        console.log(petName + " " + petType + " " + petAge);
+      }
+      else {
+        console.log("You don't get my cat's information");
+      }
+
+      if (petIsCat) {
+        console.log("My pet is a cat");
+      }
+      else {
+        console.log("My pet is not a cat");
+      }
+
+      if (petAge !== 7) {
+        console.log("My pet is not 7 years old");
+      }
+
+      if (petAge === 5) {
+        console.log("My pet is 5 years old");
+      }
+      else if (petAge < 5) {
+        console.log("My pet is less than 5 years old");
+      }
+      else {
+        console.log("My pet is older than 5 years old");
+      }
+
+      // document.write() overwrites the entire page. We don't normally use this, but we will today for simplicity
+      console.log("Welcome to our page " + userName);
+
+      var catAge = 1;
+
+      // Won't work as expected
+      console.log(catAge + 2);
+
+      // Works as expected
+      console.log(parseInt(catAge) + 2);
+
