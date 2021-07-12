2c41aa882ec282ebd8a5df9d9e944313cc2ad99a
Frank <dev.frankg@gmail.com>
2021-07-12 11:48:12 -0400

    adding another loop activity

---
+++ week-2/01-Activities/18-ZooLoop/Solved/zoo-loop-solved.js
@@ -0,0 +1,9 @@
+
+      // Array of zoo animals.
+      var zooAnimals = ["Zebra", "Rhino", "Giraffe", "Owl"];
+
+      // Loops through the array to print each zoo animal.
+      for (var i = 0; i < zooAnimals.length; i++) {
+        // Logs the animal at index position i to the console. This code is executed each we go through the loop.
+        console.log(zooAnimals[i]);
+      }
