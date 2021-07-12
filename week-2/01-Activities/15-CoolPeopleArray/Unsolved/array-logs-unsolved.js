f7722f6e5e843b8c1b9e1842f93d49ead58d6350
Frank <dev.frankg@gmail.com>
2021-07-12 09:25:18 -0400

    adding solution for js dissect and new cool people activity

---
+++ week-2/01-Activities/15-CoolPeopleArray/Unsolved/array-logs-unsolved.js
@@ -0,0 +1,12 @@
+
+    // Use this Array
+    var coolPeople = ["bruce lee", "arnold schwarzenegger", "will smith", "esther", "marie curie", "joan of arc"];
+
+    // Then use console.log to print each of the names listed inside.
+    // ...
+
+
+    // CODE GOES HERE
+
+
+    // ...
