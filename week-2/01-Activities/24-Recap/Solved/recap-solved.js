a8687bfce79b01458e110c3488ecea4b237cdb62
Frank <dev.frankg@gmail.com>
2021-07-13 10:33:17 -0400

    adding recap solution

---
+++ week-2/01-Activities/24-Recap/Solved/recap-solved.js
@@ -0,0 +1,28 @@
+
+      // Rows of Students
+      var studentsRow1 = ["Rachelle", "Jacob", "Jerome", "Greg", "Matt", "Walt"];
+      var studentsRow2 = ["Jeremiah", "Luis", "Nathan", "Analben"];
+      var studentsRow3 = ["Aisha", "Stephen", "John"];
+
+      // Entire Class
+      var entireClass = [studentsRow1, studentsRow2, studentsRow3];
+
+      // How do we print out every single student in Row 1?
+      for (var i = 0; i < studentsRow1.length; i++) {
+        console.log(studentsRow1[i]);
+      }
+
+      // How do we print out every student in Row 2 whose first name begins with J?
+      for (var j = 0; j < studentsRow2.length; j++) {
+        if (studentsRow2[j].charAt(0) === "J") {
+          console.log("Starts with a J " + studentsRow2[j]);
+        }
+      }
+
+      // Using the entireClass array, how do we access the second student in row 3?
+      console.log("Second Student in Row 3.");
+      console.log(entireClass[2][1]);
+
+      // Using the entireClass array, how do we access the third student in row 2?
+      console.log("Third student in Row 2.");
+      console.log(entireClass[1][2]);
