3f79c51e4c3192c2e9d6543678bfbb8848bde5ee
Frank <dev.frankg@gmail.com>
2021-07-13 10:37:36 -0400

    adding loop tv activity

---
+++ week-2/01-Activities/25-LoopTV/Unsolved/loop-tv-unsolved.js
@@ -0,0 +1,21 @@
+      // ????
+      var favTVshows = [];
+      var tvShow;
+
+      // For Loop Elements Repeats ??? Times
+      for (var i = 1; i < 4; i++) {
+
+        // ????
+        tvShow = "Rick & Morty";
+
+        // ????
+        favTVshows.push(tvShow);
+      }
+
+      // ????
+      for (var j = 0; j < favTVshows.length; j++) {
+
+        // ???
+        console.log(favTVshows[j]);
+      }
+
